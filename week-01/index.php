eea9762677b1743872ead9bc7bb45cf7a866b843
Eleni Ntzani <ds21116@go.uop.gr>
2025-12-08 17:43:11 +0200

    Update index.php

--- week-01/index.php
+++ week-01/index.php
@@ -1,7 +1,10 @@
 <?php
+// Create an array with simple text messages
 $messages = ["Hello!", "Welcome!", "This is a message."];
 
+// Loop through each message in the array
 foreach ($messages as $msg) {
+    // Print each message followed by a line break
     echo $msg . "<br>";
 }
 ?>
