6c5da6d29efdb2ca0bc5e940837dccc2cb108b98
Eleni Ntzani <ds21116@go.uop.gr>
2025-12-17 16:00:18 +0200

    Update models.py

--- week-02/models.py
+++ week-02/models.py
@@ -1,22 +1,22 @@
 from django.db import models
 
 class Book(models.Model):
-    entry_number = models.IntegerField()  
-    entry_date = models.DateField()       
-    author = models.CharField(max_length=255)  
-    koha_author = models.CharField(max_length=255, blank=True, null=True)  
-    title = models.CharField(max_length=255)   
-    publisher = models.CharField(max_length=255, blank=True, null=True)  
-    edition = models.CharField(max_length=255, blank=True, null=True)    
-    publish_year = models.IntegerField(blank=True, null=True)  
-    publish_place = models.CharField(max_length=255, blank=True, null=True)  
-    shape = models.CharField(max_length=255, blank=True, null=True)  
-    pages = models.CharField(max_length=50, blank=True, null=True)  
-    volume = models.CharField(max_length=50, blank=True, null=True) 
-    notes = models.TextField(blank=True, null=True)                 
-    isbn = models.CharField(max_length=50, blank=True, null=True)   
-    column1 = models.CharField(max_length=255, blank=True, null=True)  
-    column2 = models.CharField(max_length=255, blank=True, null=True)  
+    entry_number = models.IntegerField(null=True, blank=True)
+    entry_date = models.DateField(null=True, blank=True)
+    author = models.CharField(max_length=255, null=True, blank=True)
+    koha_author = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=500, null=True, blank=True)
+    publisher = models.CharField(max_length=255, null=True, blank=True)
+    edition = models.CharField(max_length=255, null=True, blank=True)
+    publish_year = models.IntegerField(null=True, blank=True)
+    publish_place = models.CharField(max_length=255, null=True, blank=True)
+    shape = models.CharField(max_length=255, null=True, blank=True)
+    pages = models.CharField(max_length=50, null=True, blank=True)
+    volume = models.CharField(max_length=100, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    isbn = models.CharField(max_length=50, null=True, blank=True)
+    column1 = models.CharField(max_length=255, null=True, blank=True)
+    column2 = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.title if self.title else "Book"
