5988c1061988ec009a37d9186aba3f3bf31cda40
Eleni Ntzani <ds21116@go.uop.gr>
2026-01-09 17:37:48 +0200

    Delete week-03/myproject directory

--- week-03/myproject/excel_data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.show_books, name='show_books'),    # root του app
-    path('add/', views.add_book, name='add_book'),
-    path('upload-excel/', views.upload_excel, name='upload_excel')
-]
