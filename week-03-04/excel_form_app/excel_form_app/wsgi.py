bdb5e13287ca81bc40b91ef6466f55141d291257
Eleni Ntzani <ds21116@go.uop.gr>
2026-01-10 16:44:42 +0200

    Update wsgi.py

--- week-03-04/excel_form_app/excel_form_app/wsgi.py
+++ week-03-04/excel_form_app/excel_form_app/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+import os  # Import the os module to interact with environment variables
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application  # Import function to get WSGI application callable
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excel_form_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excel_form_app.settings')  # Set default settings module for Django
 
-application = get_wsgi_application()
+application = get_wsgi_application()  # Create WSGI application callable used by WSGI servers
