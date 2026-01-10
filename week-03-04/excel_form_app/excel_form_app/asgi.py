edf71ac333d27024391249275ccf267c51e8473f
Eleni Ntzani <ds21116@go.uop.gr>
2026-01-10 16:40:31 +0200

    Update asgi.py

--- week-03-04/excel_form_app/excel_form_app/asgi.py
+++ week-03-04/excel_form_app/excel_form_app/asgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
-import os
+import os  # Import the os module to interact with environment variables
 
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application  # Import Django's function to get the ASGI application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excel_form_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excel_form_app.settings')  # Set the default Django settings module if not already defined
 
-application = get_asgi_application()
+application = get_asgi_application()  # Create the ASGI application callable used by the server
