4c605488370dd2ac64f9e7274948e5ebcbb8c621
Eleni Ntzani <ds21116@go.uop.gr>
2026-01-25 11:34:17 +0200

    Delete weeks-03-04/excel_form_app directory

--- weeks-03-04/excel_form_app/main/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig  # Import AppConfig to define application configuration
-
-class MainConfig(AppConfig):  # Define the configuration class for the "main" app
-    name = 'main'  # Set the name of the app
